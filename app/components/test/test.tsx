30131d33bff3a2fa657e1ce0b4e6df1d21c5f3d0
Vijay <vijayikolar@gmail.com>
2024-03-18 13:37:27 +0530

    added dropdown

--- app/components/test/test.tsx
+++
@@ -1,9 +0,0 @@
-// import { useLoaderData } from "@remix-run/react";
-// import { loader } from "~/routes/_index";
-
-export default function Test() {
-  //   const data = useLoaderData<typeof loader>();
-  //   console.log("test");
-  //   console.log(data);
-  return <div>test</div>;
-}
