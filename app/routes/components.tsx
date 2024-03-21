4281d4ede3baaf6a6720526bb3014ad6dee1599e
Vijay <vijayikolar@gmail.com>
2024-03-21 14:36:19 +0530

    added skeleton

--- app/routes/components.tsx
+++ app/routes/components.tsx
@@ -57,6 +57,7 @@ import {
   DialogFooter,
   DialogClose,
 } from "~/components/ui/dialog";
+import { Skeleton } from "~/components/ui/skeleton";
 
 export const meta: MetaFunction = () => {
   return [
@@ -70,6 +71,29 @@ export default function Index() {
   return (
     <div className="container mt-5">
       <h1 className="text-5xl text-primary mb-5">Welcome to Remix</h1>
+      <div className="my-5 w-[300px]">
+        <div>
+          <div className="flex items-center space-x-4">
+            <Skeleton className="h-12 w-12 min-w-12 rounded-full bg-red-500" />
+            <div className="space-y-2">
+              <Skeleton className="h-4 w-[250px]" />
+              <Skeleton className="h-4 w-[200px]" />
+            </div>
+          </div>
+          <div className="mt-5">
+            <div className="flex flex-col space-y-3">
+              <Skeleton className="h-[125px] w-full rounded-xl bg-green-300" />
+              <div className="space-y-2 flex gap-2">
+                <Skeleton className="h-12 w-12 rounded-full bg-green-300" />
+                <div className="flex-1">
+                  <Skeleton className="h-4 bg-green-300" />
+                  <Skeleton className="h-4  mt-2 bg-green-300" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="my-5">
         <Dialog>
           <DialogTrigger asChild>
